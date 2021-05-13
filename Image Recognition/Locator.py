ce38003f727f4a49bd57ad57bad5843a68acdd65
Steven Adi Santoso <81348080+sstevenadi@users.noreply.github.com>
2021-05-13 20:01:09 +0700

    Delete Image Recognition directory

--- Image Recognition/Locator.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-iml = pyautogui.screenshot(region=(480,840,40,40))
-iml.save(r"D:\WORK\UDINUS\ProjectPY\ImageRecognition\savedimage.png")
-
-#Pet Pals FB (975,380,450,375)
-#Food (765,75)
